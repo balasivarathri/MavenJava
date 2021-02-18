ec883e114d1987fbc7d95bed506b5660484cad37
Bala <balasivarathri@gmail.com>
2021-02-18 21:11:02 +0200

    Iterator commit

--- src/test/java/com/excel/test/ExcelReadingDataByUsingIterator.java
+++ src/test/java/com/excel/test/ExcelReadingDataByUsingIterator.java
@@ -3,40 +3,39 @@ package com.excel.test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ExcelReadingData {
+public class ExcelReadingDataByUsingIterator {
 
 	public static void main(String[] args) throws IOException {
 
-		String path = "C:\\Users\\User\\eclipse-workspace\\MavenJava\\src\\test\\java\\com\\excel\\test\\Papulation.xlsx";
-		FileInputStream fs =  new FileInputStream(path);
-		XSSFWorkbook workbook = new XSSFWorkbook(fs);
-		XSSFSheet sheet = workbook.getSheetAt(0);
-			
-		//By using for loop
-		int rows =  sheet.getLastRowNum();
-		int cols =  sheet.getRow(1).getLastCellNum();
-		for(int r=0;r<rows;r++) {
-			XSSFRow row = sheet.getRow(r);
-			for(int c=0;c<cols;c++) {
-				XSSFCell cell = row.getCell(c);
-				switch(cell.getCellType()) {
+		String location = "C:\\Users\\User\\Desktop\\Papulation.xlsx";
+		FileInputStream fi =  new FileInputStream(location);
+		XSSFWorkbook workbook = new XSSFWorkbook(fi);
+		XSSFSheet sheet = workbook.getSheet("Sheet1");
+		
+		Iterator iterator = sheet.iterator();
+		while(iterator.hasNext()) {
+			XSSFRow row =(XSSFRow) iterator.next();
+			Iterator celliterator = row.cellIterator();
+			while(celliterator.hasNext()) {
+				XSSFCell cell = (XSSFCell) celliterator.next();
+				switch(cell.getCellType()){
 				case STRING: System.out.print(cell.getStringCellValue());break;
 				case NUMERIC: System.out.print(cell.getNumericCellValue());break;
 				case BOOLEAN: System.out.print(cell.getBooleanCellValue());break;
-					
+				
 				}
 				System.out.print(" | ");
 			}
 			System.out.println();
 		}
 		
-
 	}
 
 }
